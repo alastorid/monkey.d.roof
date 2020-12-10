6f99758e3c480f724d0a7e303ad359655ac053e9
alastorid <alastorid@gmail.com>
2020-12-10 13:30:54 +0800

    Create g.js

---
+++ g.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world! test update
+// @author       You
+// @match        https://rent.591.com.tw/rent-detail-*.html
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    //kfCallName
+    //dialPhoneNum
+    var ge = (function(csName){
+        var e = document.getElementsByClassName(csName);
+        if(e.length == 1)
+        {
+            return e[0];
+        }
+        return null;
+    });
+    var puts = (function(str){
+        return ge('houseInfoTitle').textContent=str;
+    });
+    var name = ge("kfCallName")?.dataset['name'];
+    var num = ge("dialPhoneNum").dataset['value'];
+    var tel = hid_tel.value;
+    var mail = hid_email.value;
+    var msg = name + ' ' + num + ' ' + mail;
+    if('' != tel)
+    {
+         msg += ' t=' + tel;
+    }
+    puts(msg);
+    // Your code here...
+})();
